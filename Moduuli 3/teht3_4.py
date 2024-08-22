a628186c12073c70d81f210b4a92bcc4e9b26fe8
odeoodi <ode.ojala@metropolia.fi>
2024-08-22 07:15:49 +0300

    tehty

--- Moduuli 3/teht3_4.py
+++ Moduuli 3/teht3_4.py
@@ -1,6 +1,6 @@
 vuosi = int(input("Anna vuosi: "))
 
-if vuosi%4==0 or (vuosi%100==0 and vuosi%400==0):
+if (vuosi%4==0 and vuosi%100!=0) or vuosi%400==0:
     print("Vuosi on karkausvuosi")
 else:
     print("Vuosi ei ole karkausvuosi")
