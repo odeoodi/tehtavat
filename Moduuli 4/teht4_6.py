530c2b779f1a277c33e3d1bdbeb53e710b22dee6
odeoodi <ode.ojala@metropolia.fi>
2024-08-28 16:13:27 +0300

    valmis

---
+++ Moduuli 4/teht4_6.py
@@ -0,0 +1,15 @@
+import random
+
+pisteet = int(input("Pisteiden kokonaismäärä: "))
+
+ympyra = 0
+kokeiltu = 0
+
+while kokeiltu < pisteet:
+    x_koord = random.uniform(-1,1)
+    y_koord = random.uniform(-1,1)
+    if x_koord**2 + y_koord**2 < 1:
+        ympyra +=1
+    kokeiltu +=1
+
+print((ympyra*4)/kokeiltu)
