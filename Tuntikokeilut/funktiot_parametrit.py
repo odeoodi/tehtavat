de801b13dddb2261caa2d50d398480e888df9113
odeoodi <ode.ojala@metropolia.fi>
2024-09-03 16:53:34 +0300

    valmis

--- Tuntikokeilut/funktiot_parametrit.py
+++ Tuntikokeilut/funktiot_parametrit.py
@@ -1,8 +1,8 @@
-def tervehdi(tervehdys, kerrat):
+def tervehdi(kerrat, tervehdys):
     kerrat = int(input("Montako kertaa tervehditään?"))
     for i in range(kerrat):
         print(f"{tervehdys} {i+1}. kerran.")
     return
 
-tervehdi("Moro", 1)
-tervehdi("helou", 1)
+tervehdi(1, "Moi")
+tervehdi(1, "hellou")
