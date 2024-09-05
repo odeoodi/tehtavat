bb7b2e616d0c8fc03f967dc66a06cd6773ed9354
odeoodi <ode.ojala@metropolia.fi>
2024-09-05 11:33:47 +0300

    valmis

--- Moduuli 4/teht4_6.py
+++ Moduuli 4/teht4_6.py
@@ -5,7 +5,7 @@ pisteet = int(input("Pisteiden kokonaismäärä: "))
 ympyra = 0
 kokeiltu = 0
 
-while kokeiltu < pisteet:
+while kokeiltu <= pisteet:
     x_koord = random.uniform(-1,1)
     y_koord = random.uniform(-1,1)
     if x_koord**2 + y_koord**2 < 1:
