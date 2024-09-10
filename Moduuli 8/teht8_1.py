33ccce9ae4fa0fc4a74f36900a3ed4579b1ba2a8
odeoodi <ode.ojala@metropolia.fi>
2024-09-10 11:34:08 +0300

    valmis

---
+++ Moduuli 8/teht8_1.py
@@ -0,0 +1,28 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä lentoaseman ICAO-koodin. Ohjelma hakee ja
+# tulostaa koodia vastaavan lentokentän nimen ja sen sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
+# ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
+
+import mysql.connector
+
+def get_airport_by_icao(icao):
+    sql = f"select name, municipality from airport where ident = '{icao}'"
+    #print(sql)
+    cursor = db_connection.cursor()
+    cursor.execute(sql)
+    airport_data = cursor.fetchall()
+    return airport_data
+
+db_connection = mysql.connector.connect(
+         host='127.0.0.1', #host='localhost'
+         port= 3306,
+         database='flight_game',
+         user='odeo',
+         password='K4l4kukk0=!',
+         autocommit=True
+         )
+
+icao = input("Anna ICAO-koodi: ")
+airport = get_airport_by_icao(icao)
+print(airport)
+
+
