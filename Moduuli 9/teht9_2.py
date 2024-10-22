1ce4e7a72ff0e948db120ac548dbb67cef9b1619
odeoodi <ode.ojala@metropolia.fi>
2024-10-22 10:52:48 +0300

    valmis

---
+++ Moduuli 9/teht9_2.py
@@ -0,0 +1,36 @@
+"""
+Jatka ohjelmaa kirjoittamalla Auto-luokkaan kiihdytä-metodi, joka saa parametrinaan nopeuden muutoksen (km/h).
+Jos nopeuden muutos on negatiivinen, auto hidastaa. Metodin on muutettava auto-olion nopeus-ominaisuuden arvoa.
+Auton nopeus ei saa kasvaa huippunopeutta suuremmaksi eikä alentua nollaa pienemmäksi.
+Jatka pääohjelmaa siten, että auton nopeutta nostetaan ensin +30 km/h, sitten +70 km/h ja lopuksi +50 km/h.
+Tulosta tämän jälkeen auton nopeus. Tee sitten hätäjarrutus määräämällä nopeuden muutos -200 km/h ja
+tulosta uusi nopeus.
+Kuljettua matkaa ei tarvitse vielä päivittää.
+"""
+
+
+class Auto:
+    def __init__(self, rekkari, huippunopeus, nopeus_nyt=0, kuljettu_matka=0):
+        self.rekkari = rekkari
+        self.huippunopeus = huippunopeus
+        self.nopeus_nyt = nopeus_nyt
+        self.kuljettu_matka = kuljettu_matka
+
+    def kiihdyta(self, muutos):
+        if self.nopeus_nyt + muutos < 0:
+            self.nopeus_nyt = 0
+        else:
+            self.nopeus_nyt = self.nopeus_nyt + muutos
+        return
+
+
+
+auto1 = Auto("ABC-123", 142)
+
+auto1.kiihdyta(30)
+auto1.kiihdyta(70)
+auto1.kiihdyta(50)
+print(f"Auton nopeus tällä hetkellä on: {auto1.nopeus_nyt:d} km/h.")
+print("Hätäjarrutus!!!")
+auto1.kiihdyta(-200)
+print(f"Auton nopeus tällä hetkellä on: {auto1.nopeus_nyt:d} km/h.")
