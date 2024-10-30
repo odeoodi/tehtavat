88a6fd55176df81786710ecd2519e2a0545d606e
odeoodi <ode.ojala@metropolia.fi>
2024-10-30 17:52:54 +0200

    valmis

--- Moduuli 12/teht12_2.py
+++ Moduuli 12/teht12_2.py
@@ -4,6 +4,7 @@ joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa sitä vastaavan säät
 Perehdy rajapinnan dokumentaatioon riittävästi. Palveluun rekisteröityminen on tarpeen, jotta saat rajapintapyynnöissä
 tarvittavan API-avaimen (API key). Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
 '''
+from symbol import continue_stmt
 
 import requests
 import json
@@ -13,13 +14,20 @@ apikey = 'f8ac0005c4aa7003b8ba79129641b89e'
 
 
 request = f"https://api.openweathermap.org/data/2.5/forecast?q={kunta}&appid={apikey}"
-answer = requests.get(request).json()
+
+try:
+    response = requests.get(request)
+    if response.status_code==200:
+        the_response = response.json()
+except requests.exceptions.RequestException as e:
+    print("Virhe. Hakua ei voida suorittaa")
+
 
 #print(json.dumps(answer, indent=4))
 temperature = []
 
-for arvo in answer:
-    lista = answer["list"]
+for arvo in the_response:
+    lista = the_response["list"]
     for toinenarvo in lista:
         mainjuttu = toinenarvo["main"]  #Haetaan tällä "main"-sanakirjaa
         temppi = mainjuttu["temp"]
@@ -30,7 +38,7 @@ print(f"{temperature[0]-273.15:.2} Celsius")
 weather = []
 
 
-lista = answer["list"]
+lista = the_response["list"]
 for toinenarvo in lista:
     weatherkirja = toinenarvo["weather"]
     tila = weatherkirja[0]["description"]
